2ccd721e7768c00fffe62d5af37dfb3c8afdba17
Jon Mellman <jonmellman@gmail.com>
2021-01-06 20:30:55 -0800

    Modify experiments

--- experiments/src/DbClient.ts
+++ experiments/src/DbClient.ts
@@ -1,33 +1,13 @@
 /* eslint-disable quotes */
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 const connectToDatabase = async () => {
   console.log('connecting...');
+  await sleep(1000);
+  throw new Error('boom!');
 };
 const getRecord = async (message: any) => { };
 
-/* class DbClient {
-  public isConnected: boolean;
-
-  constructor() {
-    this.isConnected = false;
-  }
-
-  public async connect() {
-    if (this.isConnected) {
-      return;
-    }
-
-    await connectToDatabase();
-    this.isConnected = true;
-  }
-
-  public async getRecord(recordId: string) {
-    if (!this.isConnected) {
-      throw new Error(`DbClient can't get records before it's connected!`);
-    }
-
-    await getRecord(recordId);
-  }
-}
+/*
 
 
 class DbClient {
@@ -61,7 +41,10 @@ class DbClient {
 
   private async connect() {
     if (!this.connectionPromise) {
-      this.connectionPromise = connectToDatabase();
+      this.connectionPromise = connectToDatabase().catch(async (e) => {
+        this.connectionPromise = null;
+        throw e;
+      });
     }
 
     return this.connectionPromise;
@@ -73,22 +56,50 @@ class DbClient {
   }
 }
 
+process.on('unhandledRejection', (e) => {
+  console.log('unhandledRejection', e);
+});
+
+process.on('uncaughtException', (e) => {
+  console.log('uncaughtException', e);
+});
 
 (async () => {
   const db = new DbClient();
-  await Promise.all([
-    db.getRecord('record1'),
-    db.getRecord('record2'),
-  ]);
 
-  const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-  const myPromise = sleep(5000);
+  // await db.getRecord('record1');
+  // return;
 
-  console.time('first await');
-  await myPromise;
-  console.timeEnd('first await');
+  const myPromise = Promise.reject(new Error('boom!'));/* .catch((e) => {
+    throw new Error('from catch handler!');
+  }); */
 
-  console.time('second await');
-  await myPromise;
-  console.timeEnd('second await');
+  try {
+    await myPromise;
+  } catch (e) {
+    console.log('Caught', e);
+  }
+
+/* try {
+await db.getRecord('record1');
+} catch (e) {
+console.log('Caught error', e);
+}
+
+try {
+await db.getRecord('record2');
+} catch (e) {
+console.log('Caught error 2', e);
+} */
+
+  // const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+  // const myPromise = sleep(5000);
+
+  // console.time('first await');
+  // await myPromise;
+  // console.timeEnd('first await');
+
+  // console.time('second await');
+  // await myPromise;
+  // console.timeEnd('second await');
 })();
