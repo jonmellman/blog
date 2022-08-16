2d11f1aa4746c5cec9628bce69a0afa0c53dc73e
Jon Mellman <jonmellman@gmail.com>
2022-08-15 20:47:08 -0700

    Working

---
+++ examples/typescript-for-api-contracts/server/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+app.get('/api', async (req, res) => {
+  res.status(200).send('Done');
+})
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(3000);
+console.log('listening');
