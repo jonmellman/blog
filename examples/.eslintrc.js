197bf2c9fddb528ff3060f63433e8023f1963884
Jon Mellman <jonmellman@gmail.com>
2022-08-15 20:13:06 -0700

    Prettier works... I think

--- examples/.eslintrc.js
+++ examples/.eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'prettier'],
   root: true,
   env: {
     browser: true,
     amd: true,
     node: true
-  },
+  }
 };
