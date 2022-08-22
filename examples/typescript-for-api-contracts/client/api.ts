54300f0ffdc67f43c578be6a180aef522fe6f2e0
Jon Mellman <jonmellman@gmail.com>
2022-08-22 14:00:15 -0700

    Remove examples

--- examples/typescript-for-api-contracts/client/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-import { GetUsersApi, User } from '../shared-types/api/user';
-
-export const getUsers = async (): Promise<User[]> => {
-  const { data } = await axios.get<GetUsersApi['ResponseBody']>('/api/users');
-  return data;
-};
